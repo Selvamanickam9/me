40b565d5b3f6fd6424ba77b65d8ab47dc062abd7
Selva <selvaman2005@gmail.com>
2020-10-25 15:53:12 +0530

    Create Raylib Window

--- main.py
+++ main.py
@@ -2,4 +2,7 @@ import os
 os.environ["RAYLIB_BIN_PATH"] = "raylib-2.0.0-Win64-mingw/lib/"
 import raylibpy
 
-print("Raylib init successful")
+raylibpy.init_window(900, 500, "Hello")
+ballpos = (raylibpy.get_screen_width, raylibpy.get_screen_height)
+
+raylibpy.set_target_fps(60)
