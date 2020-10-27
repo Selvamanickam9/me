d0cb6c6ccb7e51ee9d71a3f2ee8013b9e4df4b61
Selva <selvaman2005@gmail.com>
2020-10-27 20:01:16 +0400

    Make ball move

--- Ball/main.py
+++ Ball/main.py
@@ -1,16 +1,21 @@
+import raylibpy
 import os
 os.environ["RAYLIB_BIN_PATH"] = "raylib-2.0.0-Win64-mingw/lib/"
-import raylibpy
 
 raylibpy.init_window(900, 500, "Hello")
-ballpos = (raylibpy.get_screen_width, raylibpy.get_screen_height)
+ball = raylibpy.Vector2(100, 100)
 
 raylibpy.set_target_fps(60)
 
 while not raylibpy.window_should_close():
+    if raylibpy.is_key_down(raylibpy.KEY_RIGHT):
+        ball.x += 5
+    if raylibpy.is_key_down(raylibpy.KEY_DOWN):
+        ball.y += 5
+
     raylibpy.begin_drawing()
     raylibpy.clear_background(raylibpy.Color(0, 0, 255, 255))
-    raylibpy.draw_circle(100, 100, 10.0, raylibpy.BLACK)
+    raylibpy.draw_circle(ball.x, ball.y, 10.0, raylibpy.BLACK)
     raylibpy.end_drawing()
 
 raylibpy.close_window()
